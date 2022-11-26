442527cee85c5f9a828a0860fbb24a0563c90f6d
MrSVCD <ghostdewolf@gmail.com>
2022-11-26 22:34:18 +0100

    Updated .gitignore and Build.sh

--- Build.sh
+++ Build.sh
@@ -2,4 +2,4 @@
 openscad -o "Inlet Flat.stl" "Flat.scad"
 openscad -o "Tall Side.stl" "Tall.scad"
 openscad -D 'center=true' -o "Inlet Tall Center.stl" "Tall.scad"
-
+zip -9 LazarustormFanAdapter.zip *.stl *.scad Readme.md LICENSE Build.sh
